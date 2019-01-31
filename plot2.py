45d9b0b5e464354b786aa2520960954136c3bb54
Axel <axeper@kth.se>
2019-01-31 14:32:06 +0100

    configs

---
+++ plot2.py
@@ -0,0 +1,121 @@
+#from ipywidgets import interactive, fixed
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d, Axes3D 
+import numpy as np
+import cv2
+import pickle
+
+def smpl_view_set_axis_full_body(ax,azimuth=0):
+    ## Manually set axis 
+    ax.view_init(0, azimuth)
+    max_range = 0.55
+    ax.set_xlim( - max_range,   max_range)
+    ax.set_ylim( - max_range,   max_range)
+    ax.set_zlim( -0.2 - max_range,   -0.2 + max_range)
+    ax.axis('off')
+    
+def smpl_view_set_axis_face(ax, azimuth=0):
+    ## Manually set axis 
+    ax.view_init(0, azimuth)
+    max_range = 0.1
+    ax.set_xlim( - max_range,   max_range)
+    ax.set_ylim( - max_range,   max_range)
+    ax.set_zlim( 0.45 - max_range,   0.45 + max_range)
+    ax.axis('off')
+
+im  = cv2.imread('../Thesis/infer_out_test/12.jpg')
+#print(im)
+IUV = cv2.imread('../Thesis/infer_out_test/12_IUV.png')
+INDS = cv2.imread('../Thesis/infer_out_test/12_INDS.png',  0)
+
+f = open("points.txt","r")
+collected_x = []
+collected_y = []
+collected_z = []
+i = 0
+for line in f:
+	l = line.strip("\n")
+	#print(l.split(",")[0])
+	collected_x.append(float(l.split(",")[0]))
+	collected_y.append(float(l.split(",")[1]))
+	collected_z.append(float(l.split(",")[2]))
+f.close()
+
+# Now read the smpl model.
+with open('../Thesis/smpl/models/basicmodel_m_lbs_10_207_0_v1.0.0.pkl', 'rb') as f:
+    data = pickle.load(f)
+    Vertices = data['v_template']  ##  Loaded vertices of size (6890, 3)
+    X,Y,Z = [Vertices[:,0], Vertices[:,1],Vertices[:,2]]
+
+pick_idx = 1
+# Color of each (U,V) point.
+C = np.where(INDS == pick_idx)
+# C[0] is x-coords  np.array([23,  23,   24, ..])
+# C[1] is y-coords  np.array([127, 128, 130, ..])
+print('num pts on picked person:', C[0].shape)
+#print(C[0])
+#print(C[1])
+#person_color = im[C[0], C[1], ::-1]  # boolean indexing.   ::-1 to make the RGB/BGR convention suitable for plotting.
+#print(person_color.shape)
+
+## Visualization with colors
+#fig = plt.figure(figsize=[20,10])
+
+## Visualize the full body smpl male template model and collected points
+#ax = fig.add_subplot(121, projection='3d')
+## ax.scatter(Z,X,Y,s=0.02,c='k')
+#ax.scatter(collected_z,  collected_x,collected_y,  c= person_color/255.0   )  ##s=__ size
+#smpl_view_set_axis_full_body(ax)
+#plt.title('Points on the SMPL model')
+
+## Now zoom into the face.
+#ax = fig.add_subplot(122, projection='3d')
+## ax.scatter(Z,X,Y,s=0.2,c='k')
+#ax.scatter(collected_z,  collected_x,collected_y,c=person_color/255.0) #s=__ size
+#smpl_view_set_axis_face(ax)
+#plt.title('Points on the SMPL model')
+#plt.show()
+
+#print('num pts on picked person:', C[0].shape)
+IUV_pick = IUV[C[0], C[1], :]  # boolean indexing
+IUV_pick = IUV_pick.astype(np.float)
+IUV_pick[:, 1:3] = IUV_pick[:, 1:3] / 255.0
+#print(IUV_pick.shape)
+
+print("Plotting...")
+## Visualization on the model
+fig = plt.figure(figsize=[10,5])
+
+## Visualize the full body smpl male template model and collected points
+ax = fig.add_subplot(121, projection='3d')
+ax.scatter(Z,X,Y,s=0.02,c='k')
+ax.scatter(collected_z,  collected_x,collected_y,s=25,  c=  np.arange(IUV_pick.shape[0])    )
+smpl_view_set_axis_full_body(ax)
+plt.title('Points on the SMPL model')
+
+## Now zoom into the face.
+ax = fig.add_subplot(122, projection='3d')
+ax.scatter(Z,X,Y,s=0.2,c='k')
+ax.scatter(collected_z,  collected_x,collected_y,s=55,c=np.arange(IUV_pick.shape[0]))
+smpl_view_set_axis_face(ax)
+plt.title('Points on the SMPL model')
+#
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
