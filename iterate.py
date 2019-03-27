ff6ff8fd0be1d612714229607579540908606689
Axel Persson <axeper@kth.se>
2019-03-27 14:44:01 +0100

    gif

--- iterate.py
+++ iterate.py
@@ -6,7 +6,7 @@ currentdirectory = pathlib.Path("/ImageSamples/")
 
 nr = 0
 for currentfile in currentdirectory.iterdir():
-	os.system("python3 run.py --image " + currentfile + "--output_json --input_nr" + nr)
+	os.system("python3 run.py --image " + str(currentfile) + "--output_json --input_nr" + str(nr))
 	nr += 1
 
 
